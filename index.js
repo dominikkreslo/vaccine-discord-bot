5f5b7e72b036e53572c261c8fe5540ce26758d32
Dominik Kreslo <dominik061997@gmail.com>
2021-03-24 11:11:44 -0400

    update

---
+++ index.js
@@ -0,0 +1,46 @@
+//Discord variables
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const prefix = "!";
+//Api variables
+const fetch = require("node-fetch");
+//const sendEmail = require("./email.js");
+//const sleepTime = 20 * 1000;
+const zipCode = "08755"; // must be string to maintain leading zeros
+const searchRadius = "10"; // in miles
+const terminalLink = require("terminal-link");
+const link = terminalLink(
+  "Rite Aid Appointment Website",
+  "https://www.riteaid.com/pharmacy/covid-qualifier"
+);
+//Calls api for stores
+fetch(
+  `https://www.riteaid.com/services/ext/v2/stores/getStores?address=${zipCode}&attrFilter=PREF-112&fetchMechanismVersion=2&radius=${searchRadius}`
+)
+  .then((res) => res.json())
+  .then((res) => {
+    getAvailability(res.Data.stores);
+  });
+
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+});
+
+client.login(config.BOT_TOKEN);
+
+function getAvailability(stores) {
+  let promise = [];
+  for (const store of stores) {
+    promise.push(
+      fetch(
+        `https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber=${store.storeNumber}`
+      ).then((res) => res.json())
+    );
+  }
+
+  Promise.all(promises).then((res)=>{
+    let available[]
+  })
+}
